89e604cb614ac88f5ad24a179942f8541e4ceccd
efouille <efouille@student.42.fr>
2017-04-03 16:34:49 +0200

    Tweaked nct.sh

--- nct.sh
+++ nct.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ord()
 {
@@ -7,9 +7,9 @@ ord()
 
 hlp()
 {
-    echo "\033[31mUsage :\033[0m"
-    echo "\033[32mnct -s username \033[0m     : sends stdin to username (get username with w)"
-    echo "\033[32mnct -r [file name] \033[0m  : shows messages received, if file name is specified, writes to file name and quits"
+    echo -e "\033[31mUsage :\033[0m"
+    echo -e "\033[32mnct -s username \033[0m     : sends stdin to username (get username with w)"
+    echo -e "\033[32mnct -r [file name] \033[0m  : shows messages received, if file name is specified, writes to file name and quits"
 }
 
 prt()
@@ -34,7 +34,7 @@ else
     then
         nom=$2
         port=$(prt $nom)
-        donnees=$(cat)
+        donnees=$(whoami)$'\n'$(cat)
 
         echo "$donnees" | nc 127.0.0.1 $port
     elif [ $1 = "-r" ]
@@ -48,8 +48,18 @@ else
         else
             while [ 1 -eq 1 ]
             do
-                nc -l $port
-                echo "\033[31m===== END =====\033[0m"
+                k=0
+                while read line
+                do
+                    if [ $k -eq 0 ]
+                    then
+                        echo -e "\033[31m===== Message from $line =====\033[0m"
+                    else
+                        echo "$line"
+                    fi
+                    k=$(($k + 1))
+                done < <((echo "Message sent") | nc -l $port)
+                echo -e "\033[31m===== END =====\033[0m"
             done
         fi
     fi
